614b7b3d83e65b49a108a4dc788197dd87f80b57
Francisco Javier Lisbona Roldán <javierlisbona1@gmail.com>
2024-09-05 12:09:40 +0200

    First steps with chessboard component.

--- src/app/chessboard/chessboard.component.spec.ts
+++ src/app/chessboard/chessboard.component.spec.ts
@@ -10,7 +10,7 @@ describe('ChessboardComponent', () => {
     await TestBed.configureTestingModule({
       imports: [ChessboardComponent]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(ChessboardComponent);
     component = fixture.componentInstance;
@@ -19,8 +19,49 @@ describe('ChessboardComponent', () => {
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(ChessboardComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it('should render the chessboard-app component', () => {
+    const fixture = TestBed.createComponent(ChessboardComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('chessboard-app')).toBeTruthy();
   });
+
+  it('should render a white box on odd squares', () => {
+    const fixture = TestBed.createComponent(ChessboardComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    const boxElements = compiled.querySelectorAll('.board-square') as NodeListOf<HTMLElement>;
+    boxElements.forEach((boxElement, index) => {
+      if (index % 2 !== 0) {
+        boxElement.style.backgroundColor = 'rgb(255, 255, 255)'; // Blanco en formato RGB
+      }
+
+      const backgroundColor = window.getComputedStyle(boxElement).backgroundColor;
+      expect(backgroundColor).toBe('rgb(255, 255, 255)'); // Blanco en formato RGB
+    });
+  });
+
+  it('should render a black box on even squares', () => {
+    const fixture = TestBed.createComponent(ChessboardComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    const boxElements = compiled.querySelectorAll('.board-square') as NodeListOf<HTMLElement>;
+    boxElements.forEach((boxElement, index) => {
+      if (index % 2 === 0) {
+        boxElement.style.backgroundColor = 'rgb(0, 0, 0)'; //
+      }
+      const backgroundColor = window.getComputedStyle(boxElement).backgroundColor;
+      expect(backgroundColor).toBe('rgb(0, 0, 0)'); // Negro en formato RGB
+    });
+
+  });
+
+
+
+
+
 });
--- src/app/chessboard/chessboard.component.ts
+++ src/app/chessboard/chessboard.component.ts
@@ -1,12 +1,15 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'chessboard-app',
   standalone: true,
-  imports: [],
+  imports: [NgFor,CommonModule],
   templateUrl: './chessboard.component.html',
   styleUrl: './chessboard.component.scss'
 })
 export class ChessboardComponent {
+rows: any=[`a`, `b`, `c`, `d`, `e`, `f`, `g`, `h`];
+columns: any=[`1`, `2`, `3`, `4`, `5`, `6`, `7`, `8`];
 
 }
