4eccc5ac99ad3a04fff133751623f35bfa1d06dc
Francisco Javier Lisbona Roldán <javierlisbona1@gmail.com>
2024-09-16 23:26:39 +0200

    Fix of dark square selected when clicking on it

--- src/app/chessboard/chessboard.component.ts
+++ src/app/chessboard/chessboard.component.ts
@@ -9,20 +9,41 @@ import { CommonModule } from '@angular/common';
   styleUrl: './chessboard.component.scss'
 })
 export class ChessboardComponent {
+  cols: any=[`1`, `2`, `3`, `4`, `5`, `6`, `7`, `8`];
   rows = [`a`, `b`, `c`, `d`, `e`, `f`, `g`, `h`];
-  //columns = [`1`, `2`, `3`, `4`, `5`, `6`, `7`, `8`];
-    getPiece(i: number, j: number) {
-      if (i === 0) {
-        return this.pieces[j].black;
-      } else if (i === 1) {
-        return this.pawn.black;
-      } else if (i === 6) {
-        return this.pawn.white;
-      } else if (i === 7) {
-        return this.pieces[j].white;
-      }
-      return null; // Devuelve null para cualquier otro caso
+  getPiece(i: number, j: number) {
+    if (i === 0) {
+      return this.pieces[j].black;
+    } else if (i === 1) {
+      return this.pawn.black;
+    } else if (i === 6) {
+      return this.pawn.white;
+    } else if (i === 7) {
+      return this.pieces[j].white;
     }
+    return null; // Devuelve null para cualquier otro caso
+  }
+  
+  selectedSquare: { row: number, col: number } | null = null;
+
+  onSquareClick(row: number, col: number): void {
+    if (this.isWhitePiece(row, col)) {
+      this.selectedSquare = { row, col };
+    } else {
+      this.selectedSquare = null;
+    }
+  }
+
+  isSelected(row: number, col: number): boolean {
+    return this.selectedSquare?.row === row && this.selectedSquare?.col === col;
+  }
+  
+  isWhitePiece(row: number, col: number): boolean {
+    return row === 6 || row === 7; // Las piezas blancas están en las filas 6 y 7
+  }
+  
+  // ... resto del código existente ...
+  
   pieces = [
     { white: '&#9814;', black: '&#9820;', name: 'Torre' },
     { white: '&#9816;', black: '&#9822;', name: 'Caballo' },    
